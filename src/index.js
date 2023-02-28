902774bec0dd1c46f3532d8bea81e668317083c9
Woncheol Kim <icijiy@gmail.com>
2023-02-28 12:34:04 -0800

    Created routes & component files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './component/App';
 import firebase from './firebase';
 console.log(firebase);
 
