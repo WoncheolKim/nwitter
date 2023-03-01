4b6a2153d8eb430b82d21e76b37f05b58ac0997f
Woncheol Kim <icijiy@gmail.com>
2023-03-01 10:14:15 -0800

    Login Form & Auth

--- src/fbase.js
+++ src/fbase.js
@@ -12,4 +12,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID,
   };
   
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './component/App';
-import firebase from './firebase';
-console.log(firebase);
+import App from 'components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
