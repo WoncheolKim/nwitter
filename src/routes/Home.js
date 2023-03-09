14695e63c227faa4db012e1d4c18c23130263b81
Woncheol Kim <icijiy@gmail.com>
2023-03-09 12:32:21 -0800

    Practice to save to database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,30 @@
-import React from 'react';
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = ()  => {
+    const [nwtter, setNweet] = useState("");
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("nweets").add({
+            nweet,
+            createdAt: Data.now(),
+        });
+        setNweet("");
+    };
+    const onChange = (event) => {;
+        const{
+            target: { value },
+        } = event;
+        setNweet(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={nweer} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    )
+}
+
 export default Home;
