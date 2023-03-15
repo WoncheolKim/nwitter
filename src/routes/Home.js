082640820987d3f5461fa4c17308389f9ff81073
Woncheol Kim <icijiy@gmail.com>
2023-03-15 09:51:02 -0700

    Delete and Update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { dbService } from '../fbase';
 import React, { useState, useEffect } from 'react';
+import Nweet from '../components/Nweet';
 
 const Home = ({ userObj })  => {
     const [nweet, setNweet] = useState("");
@@ -36,9 +37,11 @@ const Home = ({ userObj })  => {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                  <Nweet 
+                    key={nweet.id} 
+                    nweetObj={nweet} 
+                    isOwner={nweet.creatorId === userObj.uid}
+                  />
                 ))}
             </div>
         </div>
