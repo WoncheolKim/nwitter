b3f5fb2f9246beadb937efa30b8ca2291c21bbc6
Woncheol Kim <icijiy@gmail.com>
2023-03-24 12:08:39 -0700

    Updates

---
+++ precache-manifest.c62406df9f81489f2c334932b5bd8140.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d979e47cf8b47d68b664",
+    "url": "/nwitter/static/css/main.891f4afc.chunk.css"
+  },
+  {
+    "revision": "d979e47cf8b47d68b664",
+    "url": "/nwitter/static/js/main.d979e47c.chunk.js"
+  },
+  {
+    "revision": "544260ee2a7914c1acbe",
+    "url": "/nwitter/static/js/1.544260ee.chunk.js"
+  },
+  {
+    "revision": "09f57dde8cca147bf378",
+    "url": "/nwitter/static/js/runtime~main.09f57dde.js"
+  },
+  {
+    "revision": "7e658179f035d03037a1afa090472301",
+    "url": "/nwitter/index.html"
+  }
+];
